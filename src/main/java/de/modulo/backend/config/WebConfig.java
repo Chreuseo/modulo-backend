dcee02343cd29985563d460c7e7d89b06ca8ab3f
Christoph Euskirchen <mail@christopheuskirchen.de>
2024-09-18 10:56:18 +0200

    WebConfig

--- src/main/java/de/modulo/backend/config/WebConfig.java
+++ src/main/java/de/modulo/backend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("api/**")
                 .allowedOrigins("http://localhost:4200") // Angular app URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
