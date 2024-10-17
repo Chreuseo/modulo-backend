5a6142dd6dfda1b9db31c05fe33ebfa76f93796d
Christoph Euskirchen <mail@christopheuskirchen.de>
2024-10-17 13:54:22 +0200

    SPO Generation

--- src/main/java/de/modulo/backend/pdf/services/SpoPdfService.java
+++ src/main/java/de/modulo/backend/pdf/services/SpoPdfService.java
@@ -1,19 +1,18 @@
 package de.modulo.backend.pdf.services;
 
 import com.itextpdf.html2pdf.HtmlConverter;
-import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.IBlockElement;
-import com.itextpdf.layout.element.IElement;
-import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.*;
 import com.itextpdf.layout.properties.TextAlignment;
-import de.modulo.backend.entities.ParagraphEntity;
-import de.modulo.backend.entities.SpoEntity;
+import de.modulo.backend.dtos.ModuleFrameDTO;
+import de.modulo.backend.dtos.ModuleFrameSetDTO;
+import de.modulo.backend.entities.*;
 import de.modulo.backend.repositories.ParagraphRepository;
 import de.modulo.backend.repositories.SpoRepository;
+import de.modulo.backend.services.ModuleFrameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,12 +25,15 @@ public class SpoPdfService {
 
     private final SpoRepository spoRepository;
     private final ParagraphRepository paragraphRepository;
+    private final ModuleFrameService moduleFrameService;
 
     @Autowired
     public SpoPdfService(SpoRepository spoRepository,
-                         ParagraphRepository paragraphRepository){
+                         ParagraphRepository paragraphRepository,
+                         ModuleFrameService moduleFrameService){
         this.spoRepository = spoRepository;
         this.paragraphRepository = paragraphRepository;
+        this.moduleFrameService = moduleFrameService;
     }
 
     public ByteArrayOutputStream generateSpo(Long spoId) {
@@ -86,29 +88,72 @@ public class SpoPdfService {
             }
 
             if(spoEntity.getFooter() != null){
-                PdfCanvas pdfCanvas = new PdfCanvas(pdf.getLastPage());
-                pdfCanvas.setLineWidth(1);
-                pdfCanvas.setStrokeColor(ColorConstants.BLACK);
-                // Draw the horizontal line
-                float yPosition = paragraph.getRenderer().getOccupiedArea().getBBox().getBottom();
-                pdfCanvas.moveTo(40, yPosition); // Start Point
-                pdfCanvas.lineTo(pdf.getDefaultPageSize().getWidth() - 40, yPosition); // End Point
-                pdfCanvas.stroke(); // Execute the drawing
-
                 paragraph = getParagraphFromHtmlString(spoEntity.getFooter());
                 paragraph.setFontSize(11);
                 paragraph.setTextAlignment(TextAlignment.CENTER);
                 paragraph.setPaddingTop(24);
                 document.add(paragraph);
+            }
 
-                pdfCanvas = new PdfCanvas(pdf.getLastPage());
-                pdfCanvas.setLineWidth(1);
-                pdfCanvas.setStrokeColor(ColorConstants.BLACK);
-                // Draw the horizontal line
-                yPosition = paragraph.getRenderer().getOccupiedArea().getBBox().getBottom();
-                pdfCanvas.moveTo(40, yPosition); // Start Point
-                pdfCanvas.lineTo(pdf.getDefaultPageSize().getWidth() - 40, yPosition); // End Point
-                pdfCanvas.stroke(); // Execute the drawing
+            pdf.addNewPage(PageSize.A4.rotate());
+
+            Table table = new Table(new float[]{1, 5, 2, 2, 4, 4, 3, 3});
+            table.setWidth(800);
+            table.setFixedLayout();
+            table.setFontSize(7);
+            table.setTextAlignment(TextAlignment.CENTER);
+            table.addHeaderCell("1");
+            table.addHeaderCell("2");
+            table.addHeaderCell("3");
+            table.addHeaderCell("4");
+            table.addHeaderCell("5");
+            table.addHeaderCell("6");
+            table.addHeaderCell("7");
+            table.addHeaderCell("8");
+
+            Cell cell = new Cell(2, 1);
+            cell.add(new Paragraph("lfd. Nr."));
+            table.addCell(cell);
+            cell = new Cell(1, 3);
+            cell.add(new Paragraph("Lehrveranstaltungen"));
+            cell.setBold();
+            table.addCell(cell);
+            cell = new Cell(1, 4);
+            cell.add(new Paragraph("Prüfungen"));
+            cell.setBold();
+            table.addCell(cell);
+            table.addCell("Module");
+            table.addCell("SWS");
+            table.addCell("Art der Lehrveranstaltung");
+            table.addCell("Art");
+            table.addCell("Umfang");
+            table.addCell("Gewicht der Endnote für die Prüfungsgesamtnote");
+            table.addCell("Leistungspunkte (ECTS)");
+
+            document.add(table);
+
+            ModuleFrameSetDTO moduleFrameSetDTO = moduleFrameService.getModuleFrameSetDTOBySpoId(spoId);
+
+            int moduleCounter = 0;
+            int sectionCounter = 0;
+            for(ModuleFrameSetDTO.Section section : moduleFrameSetDTO.getSections()) {
+                if (!section.getModuleTypes().isEmpty()) {
+                    String headline = (++sectionCounter) + ". " + section.getName();
+                    paragraph = new Paragraph(headline);
+                    document.add(paragraph);
+                }
+                int moduleTypeCounter = 0;
+                for (ModuleFrameSetDTO.Section.ModuleType moduleType : section.getModuleTypes()) {
+                    if (!moduleType.getModuleFrames().isEmpty()) {
+                        String headline = sectionCounter + "." + (++moduleTypeCounter) + ". " + moduleType.getName();
+                        paragraph = new Paragraph(headline);
+                        document.add(paragraph);
+
+
+                        for (ModuleFrameDTO moduleFrame : moduleType.getModuleFrames()) {
+                        }
+                    }
+                }
             }
 
             document.close();
