905bb68fdbc4b15f5a752d14ae62dcff32eb9ac6
Christoph Euskirchen <mail@christopheuskirchen.de>
2024-10-17 11:13:13 +0200

    SPO Generation

--- src/main/java/de/modulo/backend/pdf/services/SpoPdfService.java
+++ src/main/java/de/modulo/backend/pdf/services/SpoPdfService.java
@@ -1,8 +1,10 @@
 package de.modulo.backend.pdf.services;
 
 import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.kernel.colors.ColorConstants;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.IBlockElement;
 import com.itextpdf.layout.element.IElement;
@@ -60,11 +62,13 @@ public class SpoPdfService {
             paragraph.setTextAlignment(TextAlignment.CENTER);
             document.add(paragraph);
 
-            paragraph = getParagraphFromHtmlString(spoEntity.getHeader());
-            paragraph.setFontSize(11);
-            paragraph.setTextAlignment(TextAlignment.CENTER);
-            paragraph.setPaddingTop(24);
-            document.add(paragraph);
+            if(spoEntity.getHeader() != null) {
+                paragraph = getParagraphFromHtmlString(spoEntity.getHeader());
+                paragraph.setFontSize(11);
+                paragraph.setTextAlignment(TextAlignment.CENTER);
+                paragraph.setPaddingTop(24);
+                document.add(paragraph);
+            }
 
             for(ParagraphEntity paragraphEntity : paragraphEntities){
                 paragraph = new Paragraph();
@@ -79,6 +83,23 @@ public class SpoPdfService {
                 document.add(paragraph);
             }
 
+            if(spoEntity.getFooter() != null){
+                PdfCanvas pdfCanvas = new PdfCanvas(pdf.addNewPage());
+                pdfCanvas.setLineWidth(1);
+                pdfCanvas.setStrokeColor(ColorConstants.BLACK);
+                // Draw the horizontal line
+                float yPosition = pdf.getDefaultPageSize().getHeight() - 150; // Y-position of the line
+                pdfCanvas.moveTo(40, yPosition); // Start Point
+                pdfCanvas.lineTo(pdf.getDefaultPageSize().getWidth() - 40, yPosition); // End Point
+                pdfCanvas.stroke(); // Execute the drawing
+
+                paragraph = getParagraphFromHtmlString(spoEntity.getFooter());
+                paragraph.setFontSize(11);
+                paragraph.setTextAlignment(TextAlignment.CENTER);
+                paragraph.setPaddingTop(24);
+                document.add(paragraph);
+            }
+
             document.close();
         } catch (Exception e) {
             throw new RuntimeException("Error while generating pdf.", e);
@@ -89,6 +110,9 @@ public class SpoPdfService {
 
     private Paragraph getParagraphFromHtmlString(String htmlString) {
         Paragraph paragraph = new Paragraph();
+        if(htmlString == null){
+            return null;
+        }
         for(IElement element : HtmlConverter.convertToElements(htmlString)) {
             if(element instanceof IBlockElement) {
                 paragraph.add((IBlockElement) element);
