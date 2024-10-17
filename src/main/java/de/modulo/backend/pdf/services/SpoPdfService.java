d0c380c74f8ebe75e3373e0e7cd99e2e49a05e72
Christoph Euskirchen <mail@christopheuskirchen.de>
2024-10-17 14:45:26 +0200

    SPO Generation

--- src/main/java/de/modulo/backend/pdf/services/SpoPdfService.java
+++ src/main/java/de/modulo/backend/pdf/services/SpoPdfService.java
@@ -10,8 +10,7 @@ import com.itextpdf.layout.properties.TextAlignment;
 import de.modulo.backend.dtos.ModuleFrameDTO;
 import de.modulo.backend.dtos.ModuleFrameSetDTO;
 import de.modulo.backend.entities.*;
-import de.modulo.backend.repositories.ParagraphRepository;
-import de.modulo.backend.repositories.SpoRepository;
+import de.modulo.backend.repositories.*;
 import de.modulo.backend.services.ModuleFrameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,14 +25,20 @@ public class SpoPdfService {
     private final SpoRepository spoRepository;
     private final ParagraphRepository paragraphRepository;
     private final ModuleFrameService moduleFrameService;
+    private final CourseTypeModuleFrameRepository courseTypeModuleFrameRepository;
+    private final ExamTypeRepository examTypeRepository;
+    private final ExamTypeModuleFrameRepository examTypeModuleFrameRepository;
 
     @Autowired
     public SpoPdfService(SpoRepository spoRepository,
                          ParagraphRepository paragraphRepository,
-                         ModuleFrameService moduleFrameService){
+                         ModuleFrameService moduleFrameService, CourseTypeModuleFrameRepository courseTypeModuleFrameRepository, ExamTypeRepository examTypeRepository, ExamTypeModuleFrameRepository examTypeModuleFrameRepository){
         this.spoRepository = spoRepository;
         this.paragraphRepository = paragraphRepository;
         this.moduleFrameService = moduleFrameService;
+        this.courseTypeModuleFrameRepository = courseTypeModuleFrameRepository;
+        this.examTypeRepository = examTypeRepository;
+        this.examTypeModuleFrameRepository = examTypeModuleFrameRepository;
     }
 
     public ByteArrayOutputStream generateSpo(Long spoId) {
@@ -100,47 +105,46 @@ public class SpoPdfService {
             document.add(new AreaBreak());
             document.setMargins(20, 20, 20, 20);
 
-
-            Table table = new Table(new float[]{1, 5, 2, 2, 4, 4, 3, 3});
-            table.setWidth(800);
-            table.setFixedLayout();
-            table.setFontSize(7);
-            table.setTextAlignment(TextAlignment.CENTER);
-            table.addHeaderCell("1");
-            table.addHeaderCell("2");
-            table.addHeaderCell("3");
-            table.addHeaderCell("4");
-            table.addHeaderCell("5");
-            table.addHeaderCell("6");
-            table.addHeaderCell("7");
-            table.addHeaderCell("8");
-
-            Cell cell = new Cell(2, 1);
-            cell.add(new Paragraph("lfd. Nr."));
-            table.addCell(cell);
-            cell = new Cell(1, 3);
-            cell.add(new Paragraph("Lehrveranstaltungen"));
-            cell.setBold();
-            table.addCell(cell);
-            cell = new Cell(1, 4);
-            cell.add(new Paragraph("Prüfungen"));
-            cell.setBold();
-            table.addCell(cell);
-            table.addCell("Module");
-            table.addCell("SWS");
-            table.addCell("Art der Lehrveranstaltung");
-            table.addCell("Art");
-            table.addCell("Umfang");
-            table.addCell("Gewicht der Endnote für die Prüfungsgesamtnote");
-            table.addCell("Leistungspunkte (ECTS)");
-
-            document.add(table);
-
             ModuleFrameSetDTO moduleFrameSetDTO = moduleFrameService.getModuleFrameSetDTOBySpoId(spoId);
 
             int moduleCounter = 0;
             int sectionCounter = 0;
             for(ModuleFrameSetDTO.Section section : moduleFrameSetDTO.getSections()) {
+                Table table = new Table(new float[]{1, 5, 2, 2, 4, 4, 3, 3});
+                table.setWidth(800);
+                table.setFixedLayout();
+                table.setFontSize(7);
+                table.setTextAlignment(TextAlignment.CENTER);
+                table.addHeaderCell("1");
+                table.addHeaderCell("2");
+                table.addHeaderCell("3");
+                table.addHeaderCell("4");
+                table.addHeaderCell("5");
+                table.addHeaderCell("6");
+                table.addHeaderCell("7");
+                table.addHeaderCell("8");
+
+                Cell cell = new Cell(2, 1);
+                cell.add(new Paragraph("lfd. Nr."));
+                table.addCell(cell);
+                cell = new Cell(1, 3);
+                cell.add(new Paragraph("Lehrveranstaltungen"));
+                cell.setBold();
+                table.addCell(cell);
+                cell = new Cell(1, 4);
+                cell.add(new Paragraph("Prüfungen"));
+                cell.setBold();
+                table.addCell(cell);
+                table.addCell("Module");
+                table.addCell("SWS");
+                table.addCell("Art der Lehrveranstaltung");
+                table.addCell("Art");
+                table.addCell("Umfang");
+                table.addCell("Gewicht der Endnote für die Prüfungsgesamtnote");
+                table.addCell("Leistungspunkte (ECTS)");
+
+                document.add(table);
+
                 if (!section.getModuleTypes().isEmpty()) {
                     String headline = (++sectionCounter) + ". " + section.getName();
                     paragraph = new Paragraph(headline);
@@ -153,11 +157,57 @@ public class SpoPdfService {
                         paragraph = new Paragraph(headline);
                         document.add(paragraph);
 
+                        table = new Table(new float[]{1, 5, 2, 2, 4, 4, 3, 3});
+                        table.setWidth(800);
+                        table.setFixedLayout();
+                        table.setFontSize(7);
 
                         for (ModuleFrameDTO moduleFrame : moduleType.getModuleFrames()) {
+                            if(moduleFrame.getQuantity() == 1){
+                                table.addCell(++moduleCounter + "").setTextAlignment(TextAlignment.CENTER);
+                            }else if(moduleFrame.getQuantity() > 1){
+                                table.addCell(moduleCounter + "-" + ((moduleCounter += moduleFrame.getQuantity()) - 1));
+                            }
+                            table.addCell(moduleFrame.getName());
+                            table.addCell(moduleFrame.getSws() + "").setTextAlignment(TextAlignment.CENTER);
+
+                            List<CourseTypeModuleFrameEntity> courseTypeModuleFrameEntities = courseTypeModuleFrameRepository.findCourseTypeModuleFrameEntitiesByModuleFrameId(moduleFrame.getId());
+                            StringBuilder stringBuilder = new StringBuilder();
+                            for(int i = 0; i < courseTypeModuleFrameEntities.size(); i++){
+                                stringBuilder.append(courseTypeModuleFrameEntities.get(i).getCourseType().getAbbreviation());
+                                if(i < courseTypeModuleFrameEntities.size() -1){
+                                    stringBuilder.append(", ");
+                                }
+                            }
+                            table.addCell(stringBuilder.toString()).setTextAlignment(TextAlignment.CENTER);
+                            List<ExamTypeModuleFrameEntity> examTypeModuleFrameEntities = examTypeModuleFrameRepository.getExamTypeModuleFrameEntitiesByModuleFrameId(moduleFrame.getId());
+                            stringBuilder = new StringBuilder();
+                            cell = new Cell();
+                            for(int i=0; i < examTypeModuleFrameEntities.size(); i++){
+                                stringBuilder.append(examTypeModuleFrameEntities.get(i).getExamType().getAbbreviation());
+                                if(i < examTypeModuleFrameEntities.size() - 1){
+                                    if(examTypeModuleFrameEntities.get(i).isMandatory()){
+                                        stringBuilder.append(" und ");
+                                    }else{
+                                        stringBuilder.append(" oder ");
+                                    }
+                                }
+
+                                if(examTypeModuleFrameEntities.size() > 1){
+                                    cell.add(new Paragraph(examTypeModuleFrameEntities.get(i).getExamType().getAbbreviation() + ": " + examTypeModuleFrameEntities.get(i).getExamType().getLength()));
+                                }else{
+                                    cell.add(new Paragraph(examTypeModuleFrameEntities.get(i).getExamType().getLength()));
+                                }
+                            }
+                            table.addCell(stringBuilder.toString()).setTextAlignment(TextAlignment.CENTER);
+                            table.addCell(cell).setTextAlignment(TextAlignment.CENTER);
+                            table.addCell(moduleFrame.getWeight() + "").setTextAlignment(TextAlignment.CENTER);
+                            table.addCell(moduleFrame.getCredits() + "").setTextAlignment(TextAlignment.CENTER);
                         }
                     }
                 }
+
+                document.add(new AreaBreak());
             }
 
             document.close();
