c7c8a416c4b857cb4779121b38760580109166f6
Christoph Euskirchen <mail@christopheuskirchen.de>
2024-11-14 19:18:03 +0100

    Jenkinsfile Test Automation Test fail

---
+++ src/test/java/de/modulo/backend/converters/TestTest.java
@@ -0,0 +1,14 @@
+package de.modulo.backend.converters;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class TestTest {
+
+    @Test
+    void testTest(){
+        fail();
+    }
+
+}
