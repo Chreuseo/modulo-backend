cbc9dda16cfade571efbaa620e49c9555c967018
Christoph Euskirchen <mail@christopheuskirchen.de>
2024-12-04 15:43:18 +0100

    Delete Module Frame and depending examTypeModuleFrames

--- src/main/java/de/modulo/backend/repositories/ExamTypeModuleFrameRepository.java
+++ src/main/java/de/modulo/backend/repositories/ExamTypeModuleFrameRepository.java
@@ -1,11 +1,14 @@
 package de.modulo.backend.repositories;
 
 import de.modulo.backend.entities.ExamTypeModuleFrameEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ExamTypeModuleFrameRepository extends JpaRepository<ExamTypeModuleFrameEntity, ExamTypeModuleFrameEntity.ExamTypeModuleFrameId> {
     List<ExamTypeModuleFrameEntity> getExamTypeModuleFrameEntitiesByModuleFrameId(Long moduleFrameId);
+
+    @Transactional
     void deleteExamTypeModuleFrameEntitiesByModuleFrameId(Long moduleFrameId);
 }
