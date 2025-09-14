b7fbd5f9987605a2e8730759d5f63b17c61b3f4a
Christoph Euskirchen <mail@christopheuskirchen.de>
2025-09-14 16:19:33 +0200

    Pfade für Authentifizierung robuster gestalten.

--- src/main/java/de/modulo/backend/config/SecurityConfig.java
+++ src/main/java/de/modulo/backend/config/SecurityConfig.java
@@ -2,34 +2,13 @@ package de.modulo.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Configuration class for the security of the application.
  */
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .anyRequest().permitAll()  // Allow all requests without authentication
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout(LogoutConfigurer::permitAll)
-                .csrf(AbstractHttpConfigurer::disable);
-        return http.build();
-    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
--- src/main/java/de/modulo/backend/config/WebConfig.java
+++ src/main/java/de/modulo/backend/config/WebConfig.java
@@ -1,28 +1,17 @@
 package de.modulo.backend.config;
 
-import de.modulo.backend.authentication.AlreadyLoggedInInterceptor;
 import de.modulo.backend.authentication.AuthenticationInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * Configuration class for the web configuration of the application.
- */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private AuthenticationInterceptor authenticationInterceptor;
+    private final AuthenticationInterceptor authenticationInterceptor;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:9999") // Angular app URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
+    public WebConfig(AuthenticationInterceptor authenticationInterceptor) {
+        this.authenticationInterceptor = authenticationInterceptor;
     }
 
     @Override
@@ -32,3 +21,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/auth/login");
     }
 }
+
