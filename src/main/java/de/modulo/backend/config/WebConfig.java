df106b56a3295a0f6e4001f8c2e582bc55187a7a
Christoph Euskirchen <mail@christopheuskirchen.de>
2025-09-15 11:50:27 +0200

    Revert "Pfade für Authentifizierung robuster gestalten."
    This reverts commit b7fbd5f9987605a2e8730759d5f63b17c61b3f4a.

--- src/main/java/de/modulo/backend/config/WebConfig.java
+++ src/main/java/de/modulo/backend/config/WebConfig.java
@@ -1,17 +1,28 @@
 package de.modulo.backend.config;
 
+import de.modulo.backend.authentication.AlreadyLoggedInInterceptor;
 import de.modulo.backend.authentication.AuthenticationInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuration class for the web configuration of the application.
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final AuthenticationInterceptor authenticationInterceptor;
+    @Autowired
+    private AuthenticationInterceptor authenticationInterceptor;
 
-    public WebConfig(AuthenticationInterceptor authenticationInterceptor) {
-        this.authenticationInterceptor = authenticationInterceptor;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:9999") // Angular app URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 
     @Override
@@ -21,4 +32,3 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/auth/login");
     }
 }
-
